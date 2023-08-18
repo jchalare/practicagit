117947049196e2ad59965a916997de648cafbf72
jchalare <albertchalare@gmail.com>
2023-08-17 20:42:07 -0500

    Punto 7 tercer commit

--- src/main/java/co/proyecto_jchalare/proyecto/controller/Cliente.java
+++ src/main/java/co/proyecto_jchalare/proyecto/controller/Cliente.java
@@ -1,4 +1,7 @@
 package co.proyecto_jchalare.proyecto.controller;
 
 public class Cliente {
+    public getClienteNombre(){
+        return 'Concesionario Yeah';
+    }
 }
--- src/main/java/co/proyecto_jchalare/proyecto/controller/Factura.java
+++ src/main/java/co/proyecto_jchalare/proyecto/controller/Factura.java
@@ -1,4 +1,7 @@
 package co.proyecto_jchalare.proyecto.controller;
 
 public class Factura {
+    public getFactura(){
+        return 'FC-0001';
+    }
 }
